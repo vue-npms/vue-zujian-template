f24a478e3e46587ed0b346a737de4207278f2afa
mars <578595193@qq.com>
2018-05-06 20:04:39 +0800

    ui checkbox

---
+++ src/components/Checkbox/CheckboxGroup.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="_ui-checkbox-group">
+    <slot>
+      <ui-checkbox v-for="(option, index) in options" :key="index" :val="option.value" :disabled="option.disabled === true">{{ option.label || option.value }}</ui-checkbox>
+    </slot>
+  </div>
+</template>
+
+<script>
+import CheckBox from './Checkbox.vue'
+export default {
+  props: {
+    value: {
+      type: Array,
+      require: true
+    },
+    options: {
+      type: Array
+    }
+  },
+  computed: {
+    currentValue: {
+      get () {
+        return this.value
+      },
+      set (nv) {
+        this.$children.filter(item => item.$options.name === 'UiCheckbox').forEach((child) => {
+          child.checked = this.value.indexOf(child.val) !== -1
+        })
+      }
+    }
+  },
+  watch: {
+    value: {
+      handler (newVal) {
+        this.$nextTick(() => {
+          this.currentValue = newVal
+        })
+      },
+      immediate: true
+    }
+  },
+  methods: {
+    change (val) {
+      let index = this.value.indexOf(val)
+      if (index === -1) {
+        this.value.push(val)
+      } else {
+        this.value.splice(index, 1)
+      }
+      this.currentValue = this.value
+    }
+  },
+  components: {
+    'UiCheckbox': CheckBox
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "styleMix";
+  @include ui-checkbox-group;
+</style>
+
+<!--<wap-checkbox-group v-model="arr">-->
+  <!--<wap-checkbox val="111">aaaa</wap-checkbox>-->
+  <!--<wap-checkbox val="222">bbbb</wap-checkbox>-->
+<!--</wap-checkbox-group>-->
--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,51 +1,29 @@
 <template>
   <div style="padding-top: 20px">
-    <div>
-      <ui-button @click="hi(0)">default</ui-button>
-      <ui-button type="plain" @click="hi(0.5)">plain</ui-button>
-      <ui-button type="danger" @click="hi(1)">danger</ui-button>
-      <ui-button type="primary" @click="hi(2)">primary</ui-button>
-      <ui-button type="warning" @click="hi(3)">warning</ui-button>
-      <ui-button type="hollow" @click="hi(4)">hollow</ui-button>
-    </div>
-    <div style="margin-top: 20px">
-      <ui-button disabled @click="hi(5)">default</ui-button>
-      <ui-button type="plain" disabled @click="hi(5.5)">plain</ui-button>
-      <ui-button type="danger" disabled @click="hi(6)">danger</ui-button>
-      <ui-button type="primary" disabled @click="hi(7)">primary</ui-button>
-      <ui-button type="warning" disabled @click="hi(8)">warning</ui-button>
-      <ui-button type="hollow" disabled @click="hi(9)">hollow</ui-button>
-    </div>
-    <div style="margin-top: 20px">
-      <ui-button disabled loading icon="icon-loading">default</ui-button>
-    </div>
-    <div style="margin-top: 20px">
-      <ui-button size="large" style="display: block; margin-bottom: 20px">default(默认)</ui-button>
-      <ui-button style="display: block; margin-bottom: 20px">default(默认)</ui-button>
-      <ui-button size="small" style="display: block; margin-bottom: 20px">default(默认)</ui-button>
-      <ui-button size="mini" style="display: block; margin-bottom: 20px">default(默认)</ui-button>
-    </div>
-
-    <div style="margin-top: 20px">
-      <ui-button @click="hi(0)" size="large">large</ui-button>
-      <ui-button type="warning" disabled @click="hi(8)">normal</ui-button>
-      <ui-button type="danger" size="small" @click="hi(1)">small</ui-button>
-      <ui-button type="primary" size="mini" @click="hi(2)">mini</ui-button>
-    </div>
+    <ui-checkbox-group v-model="nowSelect" :options="options"></ui-checkbox-group>
+    <ui-checkbox-group v-model="nowSelect">
+      <ui-checkbox val="1">A</ui-checkbox>
+      <ui-checkbox val="2">B</ui-checkbox>
+      <ui-checkbox val="3" disabled>C</ui-checkbox>
+    </ui-checkbox-group>
+    {{ nowSelect }}
   </div>
 </template>
 
 <script>
 // or global lazy load
-import Button from '@/components/Button/Button.vue'
+import CheckboxGroup from '@/components/Checkbox/CheckboxGroup.vue'
+import Checkbox from '@/components/Checkbox/Checkbox.vue'
 export default {
-  methods: {
-    hi (num) {
-      console.log('hi', num)
+  data () {
+    return {
+      nowSelect: ['3'],
+      options: [{value: '1', label: 'A'}, {value: '2', label: 'B'}, {value: '3', label: 'C', disabled: true}]
     }
   },
   components: {
-    'UiButton': Button
+    'UiCheckboxGroup': CheckboxGroup,
+    'UiCheckbox': Checkbox
   }
 }
 </script>
