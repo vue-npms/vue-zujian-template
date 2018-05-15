acb3d5e4cf034d38b95248123616e533bc0cbbea
mars <578595193@qq.com>
2018-05-15 10:52:49 +0800

    add UiSearch

---
+++ src/components/Search/Input.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ui-input">
+    <input v-model="currentValue" :type="type" :placeholder="placeholder" :readonly="readonly" autocomplete="off" ref="input">
+    <i class="input-clear iconfont icon-roundclosefill" @click="currentValue = ''" v-show="showClear && currentValue"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiInput',
+  props: {
+    value: [String],
+    type: [String],
+    placeholder: [String],
+    readonly: [Boolean],
+    showClear: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    currentValue: {
+      get () {
+        return this.value
+      },
+      set (val) {
+        this.$emit('input', val)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "styleMix";
+  @include ui-input;
+</style>
---
+++ src/components/Search/Search.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="_ui-search">
+    <div class="search-input-wrap" :class="{'exist-cancel-text': currentValue}">
+      <form action="#" class="search-input-form" @submit.prevent="submit">
+        <i class="iconfont icon-search"></i>
+        <ui-input class="input" type="search" :placeholder="placeholder" v-model="currentValue" :readonly="fullPage" show-clear @click.native="open"></ui-input>
+      </form>
+      <span class="cancel-text" v-show="currentValue" @click="close">{{cancelText}}</span>
+    </div>
+
+    <div v-if="fullPage" v-show="showFullPage" class="ui-search ui-search-fly">
+      <div class="search-input-wrap exist-cancel-text">
+        <form action="#" class="search-input-form" @submit.prevent="submit">
+          <i class="iconfont icon-search"></i>
+          <ui-input class="input" type="search" :placeholder="placeholder" v-model="currentValue" show-clear ref="searchRef"></ui-input>
+        </form>
+        <span class="cancel-text" @click="close">{{cancelText}}</span>
+      </div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import Input from './Input.vue'
+export default {
+  props: {
+    value: [String],
+    placeholder: [String],
+    cancelText: {
+      type: String,
+      default: '取消'
+    },
+    fullPage: [Boolean]
+  },
+  data () {
+    return {
+      showFullPage: false
+    }
+  },
+  computed: {
+    currentValue: {
+      get () {
+        return this.value
+      },
+      set (val) {
+        this.$emit('input', val)
+      }
+    }
+  },
+  watch: {
+    showFullPage (newVal) {
+      setTimeout(() => {
+        if (newVal) {
+          this.$refs['searchRef'].$refs['input'].focus()
+        } else {
+          this.$refs['searchRef'].$refs['input'].blur()
+        }
+      })
+    }
+  },
+  methods: {
+    open () {
+      if (this.fullPage) {
+        this.showFullPage = true
+      }
+    },
+    close () {
+      this.currentValue = ''
+      this.showFullPage = false
+    }
+  },
+  mounted () {
+    if (this.fullPage) {
+      window.addEventListener('popstate', () => {
+        this.showFullPage && history.go(1)
+      })
+      window.addEventListener('hashchange', (e) => {
+        e.preventDefault()
+        this.showFullPage && this.close()
+      })
+    }
+  },
+  components: {
+    [Input.name]: Input
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "styleMix";
+  @include ui-search;
+</style>
